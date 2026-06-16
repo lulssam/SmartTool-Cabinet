d28f2eadac386835fa1aaf8b507a25c9ea02722c
Lu <lumelosampaio@gmail.com>
2026-06-16 18:34:27 +0100

    ficheiro sql vistas atualizado para retornar um funcionario dependendo do seu email

--- MySQL/VISTAS.sql
+++ MySQL/VISTAS.sql
@@ -10,3 +10,17 @@ SELECT  f.idFerramenta,
         f.nArmario         AS Armario
 FROM    ferramenta f
 JOIN    tipo_ferramenta tf ON f.codigo_tipo = tf.codigo;
+
+
+-- View usada pelo endpoint GET /api/funcionarios/{email}
+CREATE VIEW View_Email AS
+SELECT f.id_func, f.nomeCompleto, f.email,      
+       CASE
+         WHEN g.id_func IS NOT NULL THEN 'GESTOR'
+         WHEN t.id_func IS NOT NULL THEN 'TECNICO'
+         WHEN b.id_func IS NOT NULL THEN 'BACKOFFICE'
+       END AS cargo
+FROM funcionario f
+LEFT JOIN gestor     g ON f.id_func = g.id_func
+LEFT JOIN tecnico    t ON f.id_func = t.id_func
+LEFT JOIN backoffice b ON f.id_func = b.id_func;  
