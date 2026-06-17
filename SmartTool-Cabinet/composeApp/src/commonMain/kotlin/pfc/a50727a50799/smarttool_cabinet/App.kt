9711792ede5fedb4beca28e55e7abeaf760ae741
Lu <lumelosampaio@gmail.com>
2026-06-17 16:05:47 +0100

    login screen + wiring para testar auth

--- SmartTool-Cabinet/composeApp/src/commonMain/kotlin/pfc/a50727a50799/smarttool_cabinet/App.kt
+++ SmartTool-Cabinet/composeApp/src/commonMain/kotlin/pfc/a50727a50799/smarttool_cabinet/App.kt
@@ -14,36 +14,20 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.jetbrains.compose.resources.painterResource
+import pfc.a50727a50799.smarttool_cabinet.core.auth.AuthRepository
+import pfc.a50727a50799.smarttool_cabinet.feature.login.LoginScreen
+import pfc.a50727a50799.smarttool_cabinet.feature.login.LoginViewModel
 
 import smarttoolcabinet.composeapp.generated.resources.Res
 import smarttoolcabinet.composeapp.generated.resources.compose_multiplatform
 
 @Composable
-@Preview
-fun App() {
+
+fun App(authRepository: AuthRepository) {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
+        val viewModel = viewModel { LoginViewModel(authRepository) }
+        LoginScreen(viewModel)
     }
 }
