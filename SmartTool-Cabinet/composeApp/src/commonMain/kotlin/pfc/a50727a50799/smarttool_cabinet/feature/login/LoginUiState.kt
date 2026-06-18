8c8338e758deec137a6bf43e945811ef8c98668d
Lu <lumelosampaio@gmail.com>
2026-06-18 10:01:49 +0100

    login com google (apaguei sem querer wops)

--- SmartTool-Cabinet/composeApp/src/commonMain/kotlin/pfc/a50727a50799/smarttool_cabinet/feature/login/LoginUiState.kt
+++ SmartTool-Cabinet/composeApp/src/commonMain/kotlin/pfc/a50727a50799/smarttool_cabinet/feature/login/LoginUiState.kt
@@ -1,3 +1,11 @@
 package pfc.a50727a50799.smarttool_cabinet.feature.login
 
-data class LoginUiState()
+import pfc.a50727a50799.smarttool_cabinet.core.auth.Session
+
+data class LoginUiState(
+    val email: String = "",
+    val password: String = "",
+    val isLoading: Boolean = false,
+    val error: String? = null,
+    val sessao: Session? = null
+)
