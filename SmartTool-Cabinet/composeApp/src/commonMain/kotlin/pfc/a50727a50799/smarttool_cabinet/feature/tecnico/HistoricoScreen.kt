2cd2f89b286e1ee33def881601c91a3d3d1a2819
Lu <lumelosampaio@gmail.com>
2026-06-25 11:16:25 +0100

    chore: ignorar pasta bin/ (output do build) e remover do tracking

--- SmartTool-Cabinet/composeApp/src/commonMain/kotlin/pfc/a50727a50799/smarttool_cabinet/feature/tecnico/HistoricoScreen.kt
+++ SmartTool-Cabinet/composeApp/src/commonMain/kotlin/pfc/a50727a50799/smarttool_cabinet/feature/tecnico/HistoricoScreen.kt
@@ -31,11 +31,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-
-// Importação do Componente Partilhado
-import pfc.a50727a50799.smarttool_cabinet.feature.gestor.TopBar
-
-// Importações do teu Tema
+import pfc.a50727a50799.smarttool_cabinet.ui.TopBar
 import pfc.a50727a50799.smarttool_cabinet.ui.theme.AppTheme
 import pfc.a50727a50799.smarttool_cabinet.ui.theme.CardBorder
 import pfc.a50727a50799.smarttool_cabinet.ui.theme.ScreenBg
