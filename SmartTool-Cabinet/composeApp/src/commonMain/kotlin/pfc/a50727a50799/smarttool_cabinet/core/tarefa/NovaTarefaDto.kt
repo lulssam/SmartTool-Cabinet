c4e25e8725688d09dd76f20b335e1f03dddb505d
Lu <lumelosampaio@gmail.com>
2026-07-01 12:27:34 +0100

    Implementação da funcionalidade de criação de novas tarefas e adição do campo título
    - Adição do campo `titulo` e `prioridade` à tabela de tarefas e respetivos DTOs.
    - Implementação da lógica de criação de tarefas em `TarefasGestorViewModel` e `TarefaRemoteDataSource`.
    - Atualização da UI para permitir a submissão de novas tarefas.
    - Ajuste nas Views SQL e dados de exemplo para incluir o novo campo `titulo`.

---
+++ SmartTool-Cabinet/composeApp/src/commonMain/kotlin/pfc/a50727a50799/smarttool_cabinet/core/tarefa/NovaTarefaDto.kt
@@ -0,0 +1,19 @@
+package pfc.a50727a50799.smarttool_cabinet.core.tarefa
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class NovaTarefaDto(
+    val idGestor: Int,
+    val idTecnico: Int,
+    val titulo: String,
+    val descricao: String,
+    val prioridade: String = "NORMAL",
+    val ferramentasPermitidasId: List<FerramentaIdDto>
+)
+
+@Serializable
+data class FerramentaIdDto(
+    val codigoTipo: Int,
+    val nFerramenta: Int
+)
--- tap_ktor/src/main/kotlin/dtos/TarefaDTO.kt
+++ tap_ktor/src/main/kotlin/dtos/TarefaDTO.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class TarefaDTO(
     val idTarefa: Int,
+    val titulo: String,
     val descricao: String,
     val tecnico: String,
     val estado: String,
